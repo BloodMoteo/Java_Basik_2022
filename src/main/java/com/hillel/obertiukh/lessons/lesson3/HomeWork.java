5e5116a968ddd692e577d5bbeebcc7ebac8b9b88
Andrew <bloodheroelf@gmail.com>
2022-11-23 23:55:30 +0200

    homework China

--- src/main/java/com/hillel/obertiukh/lessons/lesson3/HomeWork.java
+++ src/main/java/com/hillel/obertiukh/lessons/lesson3/HomeWork.java
@@ -1,2 +1,15 @@
-package com.hillel.obertiukh.lessons.lesson3;public class HomeWork {
+package com.hillel.obertiukh.lessons.lesson3;
+
+public class HomeWork {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 8;
+        int c = 6;
+
+        int volume = a*b*c;
+        int lenght = 4*(a+b+c);
+
+        System.out.println("Об'єм паралелепіпеда = " + volume);
+        System.out.println("Сумарна довжина всіх сторін = " + lenght);
+    }
 }
--- src/main/java/com/hillel/obertiukh/lessons/lesson4/China.java
+++ src/main/java/com/hillel/obertiukh/lessons/lesson4/China.java
@@ -1,2 +1,37 @@
-package com.hillel.obertiukh.lessons.lesson4;public class China {
+package com.hillel.obertiukh.lessons.lesson4;
+
+public class China {
+    public static void main(String[] args) {
+       /* int livoin = 13;
+        int liluk = 24;
+        int liver = 46;
+        int liallvoin = 860;
+
+        int sumli = liallvoin * (livoin + liluk + liver);
+        System.out.println(sumli);
+
+        int minvoin = 9;
+        int minluk = 35;
+        int minver = 12;
+        double minallvoin = 860 * 1.5;
+        double minsum = minallvoin * (minvoin + minluk + minver);
+        System.out.println(minsum);
+
+        */
+        String name = "Li";
+        int a = 13;
+        int b = 24;
+        int c = 46;
+        int summ_li = 860*(a+b+c);
+        String name1 = "Min";
+        int d = 9;
+        int e = 35;
+        int f = 12;
+        double summ_min = 860*(d+e+f)*1.5;
+
+        double summ_li_min = summ_li + summ_min;
+        System.out.println("Общий урон династии " + (name) + " = " + (summ_li));
+        System.out.println("Общий урон династии " + (name1) + " = " + (summ_min));
+        System.out.println("Общий урон двух династий" + " = " + (summ_li_min));
+    }
 }
