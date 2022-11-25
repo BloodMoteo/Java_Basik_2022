32f8f436c5b3b20ed29dfa25253b810de5984862
Andrew <bloodheroelf@gmail.com>
2022-11-25 19:44:40 +0200

    homework CS good

--- src/main/java/com/hillel/obertiukh/lessons/lesson5/CounterStrike.java
+++ src/main/java/com/hillel/obertiukh/lessons/lesson5/CounterStrike.java
@@ -3,21 +3,28 @@ import java.util.Scanner;
 
 public class CounterStrike {
     public static void main(String[] args) {
-        Scanner one = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Імя першої команди: ");
-        String teamName = one.nextLine();
+        String teamName = scanner.nextLine();
         String firstName = teamName;
         System.out.println("Ввести кількість фрагів для першого гравця: ");
-        int firstFrag = one.nextInt();
+       /* if (scanner.hasNextInt()){
+            int one = scanner.nextInt();
+            System.out.println(one);
+        }else if ()
+
+        */
+        int firstFrag = scanner.nextInt();
         System.out.println("Ввести кількість фрагів для другого гравця: ");
-        int twoFrag = one.nextInt();
+        int twoFrag = scanner.nextInt();
         System.out.println("Ввести кількість фрагів для третього гравця: ");
-        int threeFrag = one.nextInt();
+        int threeFrag = scanner.nextInt();
         System.out.println("Ввести кількість фрагів для четвертого гравця: ");
-        int fourFrag = one.nextInt();
+        int fourFrag = scanner.nextInt();
         System.out.println("Ввести кількість фрагів для пятого гравця: ");
-        int fiveFrag = one.nextInt();
+        int fiveFrag = scanner.nextInt();
         int midKills = (firstFrag + twoFrag + threeFrag + fourFrag + fiveFrag)/5;
+        int kills = (firstFrag + twoFrag + threeFrag + fourFrag + fiveFrag);
 
         System.out.println();
 
@@ -36,15 +43,22 @@ public class CounterStrike {
         System.out.println("Ввести кількість фрагів для пятого гравця: ");
         int fiveFrag1 = two.nextInt();
         int midKills1 = (fiveFrag1 + twoFrag1 + threeFrag1 + fourFrag1 + firstFrag1)/5;
+        int kills1 = (fiveFrag1 + twoFrag1 + threeFrag1 + fourFrag1 + firstFrag1);
 
         double whoTop = midKills - midKills1;
+
+        boolean victory = midKills > midKills1;
+        String victoryName = victory ? teamName : teamName2;
+
+        boolean res = kills > kills1;
+        double result = res ? kills : kills1;
 
 
         System.out.println("Середнє арифметичне команди "+ firstName + " = " + midKills + " очків");
         System.out.println("Середнє арифметичне команди "+ twoName + " = " + midKills1 + " очків");
         System.out.println("Різниця між командами = "+ whoTop);
-        System.out.println("Перемогла команда " +  "набрала "   + "очків");
+        System.out.println("Перемогла команда " + victoryName + " набрала " + result + " очків");
 
     }
 }
-/*teamName*//*result*/
+
--- src/main/java/com/hillel/obertiukh/lessons/lesson6/main.java
+++ src/main/java/com/hillel/obertiukh/lessons/lesson6/main.java
@@ -1,2 +1,93 @@
-package com.hillel.obertiukh.lessons.lesson6;public class main {
+package com.hillel.obertiukh.lessons.lesson6;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        /*
+        System.out.println(0 + Integer.toBinaryString(5));
+        System.out.println(Integer.toBinaryString(9));
+        System.out.println();
+        System.out.println(5 | 9); //or
+        System.out.println(5 & 9);//and
+        System.out.println(5 ^ 9);//xor
+
+        System.out.println();
+        System.out.println(Integer.toBinaryString(13));
+        System.out.println("000" + Integer.toBinaryString(1));
+        System.out.println(Integer.toBinaryString(12));
+        System.out.println();
+        System.out.println(5 << 5);// По бітовий зсув
+        System.out.println(160 >> 5);// По бітовий зсув
+
+
+         */
+/*
+        if (){
+            System.out.println();
+        }else {
+            System.out.println();
+        }
+
+        if (){
+            System.out.println();
+        } else if () {
+            System.out.println();
+        }else {
+            System.out.println();
+        }
+
+        int a = 32;
+        int b= 22;
+
+        System.out.println((a < b) ? "YES" : "NO");// Три нарний оператор можна замість if
+
+
+        //switch
+
+        switch (a){
+            case 1:{
+                System.out.println();
+            }
+            case 2:{
+                System.out.println();
+            }
+            case n:{
+                System.out.println();
+                break;
+            }
+            //OR
+            default:{
+                System.out.println();
+                break;
+            }
+        }
+
+ */
+        ///Scanner
+
+       /* Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter some integer:");
+        if (scanner.hasNextInt()){
+            int a = scanner.nextInt();
+            System.out.println(a);
+        }
+        System.out.println("BAD VERSION");
+        System.out.println(scanner.nextLine());
+
+
+        */
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter some integer:");
+        if (scanner.hasNextInt()){
+            int a = scanner.nextInt();
+            System.out.println(a);
+        }else {
+            scanner.nextLine();
+        }
+
+
+
+    }
+
 }
